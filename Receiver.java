16a5ba66afe798062e5cbd49d94eada15f4b0692
Unknown <tsengcyen@hotmail.com>
2018-04-01 14:01:26 -0500

    Variable Placed - packetSize

--- Receiver.java
+++ Receiver.java
@@ -1,14 +1,8 @@
 import java.io.*;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.nio.ByteBuffer;
-import java.util.SortedMap;
-import java.util.TreeMap;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.net.*;
+import java.nio.*;
+import java.util.*;
+import java.util.logging.*;
 
 public class Receiver {
 
@@ -16,91 +10,62 @@ public class Receiver {
     private final static Logger audit = Logger.getLogger("requests");
     private final static Logger errors = Logger.getLogger("errors");
 
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) {
     	File output = new File("test.jpg");
-    	SortedMap <Integer, byte[]> serverByte = new TreeMap <Integer, byte[]>();
+    	SortedMap <Integer, byte[]> serverByte = new TreeMap <>();
     	byte[] data;
     	int sequence;
     	int offset;
+    	int packetSize = 1024;
     	long inLength;
-        InetAddress host = InetAddress.getByName("localhost");
     	boolean last = false;
     	boolean complete = false;
     	ByteBuffer headerBB;
     	int counter=0;
-    	long ackTime;
-    	long recTime;
-    	double bad =0.20;
-    	byte errStatus = 0;
-    	String ackStatus = "";
     	int ackno = 1;
-    	DatagramPacket ack;
-    	byte[] ackData;
-    	ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    	DataOutputStream dos = new DataOutputStream(baos);
-    	
+
         try (DatagramSocket socket = new DatagramSocket(PORT)) {
             while (true) {
                 try {
-                    DatagramPacket request = new DatagramPacket(new byte[1040], 1040);
-                    recTime = System.nanoTime();
+                    DatagramPacket request = new DatagramPacket(new byte[packetSize+17], packetSize+17);
                     socket.receive(request);
-                    recTime = System.nanoTime()-recTime;
-                    
+
                     //parse out out header
-                    data = new byte[request.getLength()-16];
+                    data = new byte[request.getLength()-17];
                     headerBB = ByteBuffer.wrap(request.getData());
                     sequence = headerBB.getInt();
                     offset = headerBB.getInt();
                     inLength = headerBB.getLong();
-                    errStatus = headerBB.get();
-                    
-                    headerBB.get(data, 0, request.getLength()-16);                    
+
+
+                    headerBB.get(data, 0, request.getLength()-17);
                     serverByte.put(sequence, data);
-                    
-                    if (ackno != sequence){
-                    	System.out.println("DUPL "+ recTime + " " + sequence +" !Seq");
-                    	continue;
-                    }
-                    else if (errStatus == 1){
-                		System.out.println("RECV "+ recTime + " " + sequence +" CRPT");
-                		continue;
-                	}
-                	else{
-                		System.out.println("RECV "+ recTime + " " + sequence +" RECV");
-                		
-                		ackStatus = dropCheck(bad);
-                		
-                		if(ackStatus.equals("DROP"))
-                			continue;
-                		else if (ackStatus.equals("SENT"))
-                    		dos.writeByte(0);
-                		else if(ackStatus.equals("ERR"))
-                			dos.writeByte(1);
-                		
-                		dos.writeInt(sequence);
-                		ackData = baos.toByteArray();
-                		ack = new DatagramPacket(ackData, 5, host,14);
-                		ackTime = System.nanoTime();
-                		socket.send(ack);
-                		
-                		System.out.println("SENDing ACK "+sequence+" "+TimeUnit.NANOSECONDS.toMicros(System.nanoTime() - ackTime)+" "+dropCheck(bad));
-                		ackno++;
-                	}
-                 
-                    
+
+                    /*
+                     *	if (ackno == sequence){
+                    		System.out.println("Receive, time, sequence no, recv");
+                    		Send window ack
+                    		ackno++;
+                    	}
+                    	else{
+                    		System.out.println("Duplicate, time, sequence no, out of order");
+                    		Send dupe ack
+                    		continue;
+                    	}
+                     */
+
                     System.out.println(sequence + "-"+offset + "-"+(offset+data.length-1));
-                    
+
                     //check if last packet has been received
                     if(inLength <= offset+data.length) {
                     	last = true;
                     }
-                    
+
                     //if last packet has been received, check that all packets have been received
                     if(last) {
                     	complete = true;
                     	for(Integer i: serverByte.keySet()) {
-                    		
+
                     		//iterate through sequence numbers, compare to counter, make sure all number are present and in order
                     		if(i-1 == counter) {
                     			counter++;
@@ -113,16 +78,16 @@ public class Receiver {
                     	}
                     	counter = 0;
                     }
-                    
+
                     //if all packets in and in order, write to file
                 	if(last && complete) {
                 		FileOutputStream fos = new FileOutputStream(output);
                 		try {
                     		for(byte[] b: serverByte.values()) {
-                    			
+
                     			fos.write(b);
                     		}
-                    		
+
                     		//close fileoutputstream, reset complete and last for server to continue receiving
                     		fos.close();
                     		complete = false;
@@ -138,14 +103,5 @@ public class Receiver {
         } catch (IOException ex) {
             errors.log(Level.SEVERE, ex.getMessage(), ex);
         }
-    }
-    private static String dropCheck (double bad) {
-    	double badCheck = Math.random();
-		if (badCheck < bad/2)
-			return "DROP";
-		else if (badCheck < bad)
-			return "ERR";
-		else
-			return "SENT";
     }
 }
--- Sender.java
+++ Sender.java
@@ -1,15 +1,11 @@
 import java.io.*;
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketTimeoutException;
-import java.nio.ByteBuffer;
-import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
+import java.net.*;
+import java.nio.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.concurrent.*;
 
-import javax.swing.JFileChooser;
+import javax.swing.*;
 
 public class Sender {
 
@@ -17,12 +13,13 @@ public class Sender {
 
     public static void main(String[] args) {
     	//File in = new File(args[0]);
-    	
+
     	ByteBuffer headerBB;
     	File in=null;
 		double bad = 0.20;
 		byte ackerr;
 		int ackno;
+		int packetSize = 1024;
 		String ackStatus ="";
 
     	//filechooser as substitue for cmd while developing
@@ -33,54 +30,54 @@ public class Sender {
 		}
 
 		long inLength = in.length();
-		
+
         try (DatagramSocket socket = new DatagramSocket(14)) {
         	ByteArrayOutputStream baos = new ByteArrayOutputStream();
         	DataOutputStream dos = new DataOutputStream(baos);
 
         	byte[] data;
         	byte[] img = Files.readAllBytes(in.toPath());
-        	byte[] buf = new byte[1040];
+        	byte[] buf = new byte[packetSize+17];
             InetAddress host = InetAddress.getByName("localhost");
             int offset = 0;
             int packetLength = (int) Math.ceil(inLength/12.0);
             DatagramPacket request;
-            
+
             //for images of smaller size, split them into 12 packets still, packet size = img size/12 + header
-            if (inLength < 1024*12) {
+            if (inLength < packetSize*12) {
             	for (int counter=1;counter<=12;counter++)
             	{
             		buf=null;
             		offset = packetLength*(counter-1);
-            		
+
             		//get data from image file, if last packet then just get remaining data instead of full packet
             		if(inLength <= offset + packetLength - 1)
             			data = Arrays.copyOfRange(img, offset, (int) inLength);
             		else
             			data = Arrays.copyOfRange(img, offset, offset+packetLength);
-            		
+
             		//add header for packet
             		dos.writeInt(counter);
             		dos.writeInt(offset);
             		dos.writeLong(inLength);
             		dos.write(data);
             		buf = baos.toByteArray();
-            		
-            		
+
+
             		System.out.println(counter + "-" + offset+ "-" +(offset+data.length-1));
-            		
+
             		//create packet and send request
             		if(inLength <= offset + packetLength - 1)
-                		request = new DatagramPacket(buf, (int) inLength-offset+16,host, PORT);
+                		request = new DatagramPacket(buf, (int) inLength-offset+17,host, PORT);
             		else
-            			request = new DatagramPacket(buf, packetLength+16,host, PORT);
-            		       
+            			request = new DatagramPacket(buf, packetLength+17,host, PORT);
+
             		long time = System.nanoTime();
             		socket.send(request);
             		//logging message for smaller files
-            		System.out.println("SENDing " + counter +  " " + offset + ":" + (offset+data.length-1) + " " 
+            		System.out.println("SENDing " + counter +  " " + offset + ":" + (offset+data.length-1) + " "
             		+ TimeUnit.NANOSECONDS.toMicros(System.nanoTime() - time) + " " + dropCheck(bad));
-            		
+
             		socket.setSoTimeout(2000);
 
             		//receiving ack and resending if drop
@@ -88,22 +85,19 @@ public class Sender {
             			try{
             				DatagramPacket ack = new DatagramPacket(new byte[5], 5);
 	            			socket.receive(ack);
-	            			
+
 	            			//parsing ack
 	                        headerBB = ByteBuffer.wrap(ack.getData());
 	                        ackerr = headerBB.get();
 	                        ackno = headerBB.getInt();
-	                        
+
 	                        if (ackerr == 1) {
 	                        	ackStatus = "ErrAck";
-	                        }
-	                        else if(ackno != counter) {
-	                        	ackStatus = "DuplAck";
 	                        }
 	                        else {
 	                        	ackStatus = "MoveWnd";
 	                        }
-	                        
+
 	                        //logging ack
 	            			System.out.println("AckRcvd " + ackno + " " + ackStatus);
             				if(ackStatus.equals("MoveWnd")){
@@ -114,77 +108,74 @@ public class Sender {
             			catch (SocketTimeoutException e) {
             				System.out.println("TimeOut: " + counter);
             			}
-            			
+
             			//resend for timeout and err acks
             			time = System.nanoTime();
             			socket.send(request);
             			//logging resend message
-            			System.out.println("ReSend" + counter +  " " + offset + ":" + (offset+data.length-1) + " " 
+            			System.out.println("ReSend" + counter +  " " + offset + ":" + (offset+data.length-1) + " "
                         		+ TimeUnit.NANOSECONDS.toMicros(System.nanoTime() - time) + " " + dropCheck(bad));
             		}
-            		
+
             		baos.reset();
             	}
             }
-            
+
             //for images of larger size, just use packet size of 1024 bytes of data + header
         	else {
             	int counter=0;
-        		while(offset + 1024 < in.length()) {
+        		while(offset + packetSize < in.length()) {
         		buf=null;
         		counter++;
-        		offset = 1024*(counter-1);
-        		
+        		offset = packetSize*(counter-1);
+
         		//get data from image file, if last packet then just get remaining data instead of full packet
-        		if(inLength <= offset + 1023)
+        		if(inLength <= offset + packetSize-1)
         			{data = Arrays.copyOfRange(img, offset, (int) inLength);}
         		else
-        			{data = Arrays.copyOfRange(img, offset, offset+1024);}
-        		
+        			{data = Arrays.copyOfRange(img, offset, offset+packetSize);}
+
         		//add header
     			dos.writeInt(counter);
     			dos.writeInt(offset);
     			dos.writeLong(inLength);
-        		
+
         		dos.write(data);
         		buf = baos.toByteArray();
-        		
+
         		System.out.println(counter + "-" + offset +"-"+(offset+data.length-1));
-        		
+
         		//create packet and send request
-        		if(inLength <= offset + 1023)
-            		request = new DatagramPacket(buf, (int) inLength-offset+16,host, PORT);
-        		else 
-            		request = new DatagramPacket(buf, 1040, host, PORT);
+        		if(inLength <= offset + packetSize-1)
+            		request = new DatagramPacket(buf, (int) inLength-offset+17,host, PORT);
+        		else
+            		request = new DatagramPacket(buf, packetSize+16, host, PORT); // packetSize+17 breaks code
 
         		long time = System.nanoTime();
-        		socket.send(request);
-        		//logging send message for larger files
-        		System.out.println("SENDing " + counter +  " " + offset + ":" + (offset+data.length-1) + " " 
-        		+ TimeUnit.NANOSECONDS.toMicros(System.nanoTime() - time) + " " + dropCheck(bad));
-        		
+                socket.send(request);
+                //logging send message for larger files
+                System.out.println("SENDing " + counter +  " " + offset + ":" + (offset+data.length-1) + " "
+                + TimeUnit.NANOSECONDS.toMicros(System.nanoTime() - time) + " " + dropCheck(bad));
+
         		socket.setSoTimeout(2000);
-        		
+
         		while(true) {
         			try{
         				DatagramPacket ack = new DatagramPacket(new byte[8], 8);
             			socket.receive(ack);
-            			
+
             			//parsing ack
                         headerBB = ByteBuffer.wrap(ack.getData());
                         ackerr = headerBB.get();
                         ackno = headerBB.getInt();
-                        
+
                         if (ackerr == 1) {
                         	ackStatus = "ErrAck";
-                        }
-                        else if(ackno != counter) {
-                        	ackStatus = "DuplAck";
                         }
                         else {
                         	ackStatus = "MoveWnd";
                         }
-            			
+
                         //loggin ack
             			System.out.println("AckRcvd " + ackno + " " + ackStatus);
         				if(ackStatus.equals("MoveWnd")){
@@ -195,12 +186,12 @@ public class Sender {
         			catch (SocketTimeoutException e) {
         				System.out.println("TimeOut: " + counter);
         			}
-        			
+
         			//resending for timeout and err ack
         			time = System.nanoTime();
         			socket.send(request);
         			//logging resend
-        			System.out.println("ReSend" + counter +  " " + offset + ":" + (offset+data.length-1) + " " 
+        			System.out.println("ReSend" + counter +  " " + offset + ":" + (offset+data.length-1) + " "
                     		+ TimeUnit.NANOSECONDS.toMicros(System.nanoTime() - time) + " " + dropCheck(bad));
         		}
         		baos.reset();
@@ -210,7 +201,7 @@ public class Sender {
             ex.printStackTrace();
         }
 }
-    
+
     private static String dropCheck (double bad) {
     	double badCheck = Math.random();
 		if (badCheck < bad/2)
@@ -220,5 +211,5 @@ public class Sender {
 		else
 			return "SENT";
     }
-    
+
 }
